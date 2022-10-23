533fd2f2306b1f92e38f072ea69e5a43f657c8d3
arroyave-yohan <arroyaveyohan@gmail.com>
2022-10-23 17:06:07 -0500

    Commiting changes for the DB to run

--- giftCardApi/models.py
+++ giftCardApi/models.py
@@ -4,12 +4,12 @@ from django.contrib.auth.models import User
 
 class giftCard(models.Model):
     client = models.EmailField(max_length = 64, blank = False, null = False)
-    #provider = models.CharField(max_length = 64, blank = False, null = False)
+    provider = models.CharField(max_length = 64, blank = False, null = False, default = 'arroyaveCard')
     balance = models.IntegerField(blank = False, null = False)
-    #redemptionToken = models.CharField(max_length = 64, blank = False)
-    #redemptionCode = models.CharField(max_length = 64, blank = False)
-    #emissionDate = models.CharField(max_length = 64, blank = False)
-    #expiringDate = models.CharField(max_length = 64, blank = False)
+    redemptionToken = models.CharField(max_length = 64, blank = False, null = False)
+    redemptionCode = models.CharField(max_length = 64, blank = False)
+    emissionDate = models.CharField(max_length = 64, blank = False, null = False)
+    expiringDate = models.CharField(max_length = 64, blank = False, null = False)
     
     def __str__(self):
         return self.task
